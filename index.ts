ad03e932e8c272ef7cdaefbfb76ef84ac73a5e06
Kainatamin <Kainatamin2019@gmail.com>
2024-04-16 22:21:23 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter your first number", type: "number", name: "firstNumber" },
+  { message: "enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one operators to perform action ",
+    type: "list",
+    name: "operator",
+    choices: ["ADDITION", "SUBTRACTION", "MULTIPILICATION", "DIVISION"],
+  },
+]);
+
+//   CONDITIONAL STATEMENT
+if (answer.operator === "ADDITION"){
+    console.log(answer.firstNumber + answer.secondNumber);
+
+} else if(answer.operator === "SUBTRACTION"){
+  console.log(answer.firstNumber - answer.secondNumber);
+
+}  else if(answer.operator === "MULTIPILICATION"){
+  console.log(answer.firstNumber * answer.secondNumber);
+
+}  else if(answer.operator === "DIVISION"){
+  console.log(answer.firstNumber / answer.secondNumber);
+
+}  
+else{"PLease select correct operator"}
